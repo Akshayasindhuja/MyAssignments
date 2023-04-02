ab273e4c6987348f98bd7efd3e467bc46d876da8
Akshayasindhuja <129660240+Akshayasindhuja@users.noreply.github.com>
2023-04-02 17:56:26 +0530

    Add files via upload
    day1

---
+++ day1/Iphone.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class Iphone {
+	
+	public void makeCall() {
+		System.out.println("Trying to make a Call.");
+	}
+	public void model() {
+		System.out.println("Model of Iphone.");
+	}
+}
---
+++ day1/Smartphone.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class Smartphone {
+
+	public static void main(String[] args) {
+		
+		Iphone handset = new Iphone();
+		handset.makeCall();
+		handset.model();
+	}
+}
