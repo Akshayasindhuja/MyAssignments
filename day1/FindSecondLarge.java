097c2ad1fa503fe9db8640c5f5bf6435dd14e0db
Akshayasindhuja <129660240+Akshayasindhuja@users.noreply.github.com>
2023-04-21 12:56:09 +0530

    day3

---
+++ day1/FindSecondLarge.java
@@ -0,0 +1,15 @@
+package week2.day1;
+
+import java.util.Arrays;
+
+public class FindSecondLarge {
+
+	public static void main(String[] args) {
+		int[] digits = {21,17,11,55,69,47};
+		int length = digits.length;
+		Arrays.sort(digits);		
+			for (int i = 0; i < digits.length; i++) {		
+}
+			System.out.println(+digits[length-2]);
+}
+}
