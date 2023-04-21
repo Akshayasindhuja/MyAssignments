bdf6ad79cbc9510530a2337a1a348a6f77000efa
Akshayasindhuja <129660240+Akshayasindhuja@users.noreply.github.com>
2023-04-21 12:55:33 +0530

    day2

---
+++ day2/Classroomactivity2.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class Classroomactivity2 {
+
+	public static void main(String[] args) {
+		int number = 565378;
+		if(number%2==0) {
+			System.out.println("even number "+ number);
+		}else if(number%2!=0) {
+			System.out.println("odd number"+number);
+		}else {
+			System.out.println("Nothing"+number);
+
+	}
+	}
+}
---
+++ day2/Mobile.java
@@ -0,0 +1,23 @@
+package week1.day2;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		String mobileBrandName = "Samsung";
+		char mobileLogo = 'S';
+		short noOfMobilePiece = 50;
+		int modelNumber = 12456;
+		long mobileIMEINumber = 1245675743241567890L;
+		float mobilePrice = 14399.25f;
+		boolean isDamaged = false;
+		System.out.println("My mobile's brand is "+mobileBrandName+"\n"
+				+ "My mobile's logo is "+mobileLogo+"\n"
+				+ "The No of pieces of my mobile left in the market are "+noOfMobilePiece+"\n"
+				+ "My Mobile's Brand Name is SamsungA"+modelNumber+"\n"
+				+ "My Mobile's IMEI number is "+mobileIMEINumber+"\n"
+				+ "My Mobile's price is Rs."+mobilePrice+"\n"
+				+ "Is my mobile phone damaged? "+isDamaged);
+
+	}
+
+}
