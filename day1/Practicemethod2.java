df9117dda7f5d896e2db58ca1c0624f86ce82168
Akshayasindhuja <129660240+Akshayasindhuja@users.noreply.github.com>
2023-04-21 12:57:53 +0530

    day1

---
+++ day1/Practicemethod2.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Practicemethod2 {
+	
+	public void laptop() {
+		System.out.println("I am creating a statement for the laptop.");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Practicemethod2 Lenovo = new Practicemethod2();
+Lenovo.laptop();
+	}
+
+}
---
+++ day1/Remotework.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Remotework {
+
+	public void workfromHome() {
+		System.out.println("I am searcing for the job to do work from home.");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Remotework Testing = new Remotework();
+			Testing.workfromHome();
+	}
+	
+	}
