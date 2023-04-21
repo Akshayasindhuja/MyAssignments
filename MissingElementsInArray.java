d080e82e10c7f9261b179741e2da4ef58bd80c0a
Akshayasindhuja <129660240+Akshayasindhuja@users.noreply.github.com>
2023-04-21 13:09:59 +0530

    day3

---
+++ MissingElementsInArray.java
@@ -0,0 +1,18 @@
+package week2.day3;
+
+import java.util.Arrays;
+
+public class MissingElementsInArray {
+
+	public static void main(String[] args) {
+		int[] Array = {1,2,3,4,5,6,8};
+		int length = Array.length;
+		Arrays.sort(Array);		
+			for (int i = 0; i < Array.length; i++) {	
+				int j = i+1;
+				if (Array[i]!=j) {
+					System.out.println("The Missing element is :"+j+" "+ "Length is "+length);
+				}
+			}
+		}
+}
